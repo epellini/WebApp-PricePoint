09e505c4163f49cf5b6b1e09eaeafa9dd44a6214
Enrico Pellini <142680134+epellini@users.noreply.github.com>
2024-03-27 16:12:26 -0400

    add supabase

---
+++ src/pages/Tab1.jsx
@@ -0,0 +1,354 @@
+import React, { useEffect, useState } from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonSearchbar,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonItemOptions,
+  IonItemOption,
+  IonItemSliding,
+  IonLabel,
+  IonBadge,
+  IonText,
+  IonAlert,
+  IonButton,
+  IonFab,
+  IonFabList,
+  IonFabButton,
+  IonToast,
+  IonInput,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
+import { create, cart, trash, add } from "ionicons/icons";
+
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+const Tab1 = () => {
+  const [editItem, setEditItem] = useState();
+  const [inputName, setInputName] = useState("");
+  const [inputPrice, setInputPrice] = useState("");
+  const [items, setItems] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showAddAlert, setShowAddAlert] = useState(false);
+  const [tempInputName, setTempInputName] = useState("");
+  const [tempInputPrice, setTempInputPrice] = useState("");
+  const [showEditAlert, setShowEditAlert] = useState(false);
+  const [selectedItemId, setSelectedItemId] = useState(null);
+
+  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
+  const [itemToDelete, setItemToDelete] = useState(null);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [showToast, setShowToast] = useState(false);
+
+  const handleInput = (value) => {
+    setSearchTerm(value.toLowerCase());
+    console.log(value);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, [searchTerm]);
+
+  const loadData = async () => {
+    try {
+      let { data, error } = await supabase.from("products").select("*");
+
+      if (error) throw error;
+
+      const sortedData = data.sort((a, b) =>
+        a.product_name.localeCompare(b.product_name)
+      );
+
+      setItems(sortedData ?? []);
+    } catch (error) {
+      alert(error.message);
+      setItems([]);
+    }
+  };
+
+  const addToBasket = (price) => {
+    setTotalPrice((currentTotal) => currentTotal + price);
+    setShowToast(true);
+  };
+
+  const formatPrice = (price) => {
+    return new Intl.NumberFormat("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(price);
+  };
+
+  const updateProductPrice = async (itemId, newPrice) => {
+    try {
+      const { data, error } = await supabase
+        .from("products")
+        .update({
+          product_price: parseFloat(newPrice),
+          product_updateDate: new Date(),
+        })
+        .match({ id: itemId });
+
+      if (error) throw error;
+
+      // After updating, reload the items to reflect the changes
+      loadData();
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const addItem = async (productName, productPrice) => {
+    try {
+      let { error } = await supabase
+        .from("products")
+        .insert([
+          {
+            product_name: productName,
+            product_price: parseFloat(productPrice),
+          },
+        ]);
+
+      if (error) throw error;
+
+      // Reload items after adding
+      loadData(); // Assumes loadData is defined as before to fetch items
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const confirmDelete = (itemId) => {
+    setItemToDelete(itemId); // Store the item ID to delete
+    setShowDeleteAlert(true); // Show the delete confirmation alert
+  };
+
+  const deleteItem = async (itemId) => {
+    try {
+      let { error } = await supabase
+        .from("products")
+        .delete()
+        .match({ id: itemId });
+
+      if (error) throw error;
+
+      // Reload items after deletion
+      loadData(); // Assumes loadData is defined as before to fetch items
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Toolbar</IonTitle>
+        </IonToolbar>
+        <IonToolbar>
+          <IonSearchbar
+            debounce={1000}
+            onIonChange={(e) => handleInput(e.detail.value)}
+          ></IonSearchbar>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Productos</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonFab slot="fixed" vertical="bottom" horizontal="end">
+          <IonFabButton onClick={() => setShowAddAlert(true)}>
+            <IonIcon icon={add}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+
+        <IonToast
+          trigger="open-toast"
+          message="This toast will disappear after 5 seconds"
+          duration={5000}
+        ></IonToast>
+
+        <IonList inset={true}>
+          {items
+            .filter((item) =>
+              item.product_name.toLowerCase().includes(searchTerm)
+            )
+            .map((product, index) => (
+              <IonItemSliding key={index}>
+                <IonItem button={true}>
+                  <IonLabel>
+                    <h2>{product.product_name}</h2>
+                    <IonText color="medium" style={{ fontSize: "smaller" }}>
+                      <p>
+                        {new Date(
+                          product.product_updateDate
+                        ).toLocaleDateString("es-ES", {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        })}
+                      </p>
+                    </IonText>
+                  </IonLabel>
+                  <IonBadge slot="end" color="success">
+                    $ {formatPrice(product.product_price)}
+                  </IonBadge>
+                </IonItem>
+                <IonItemOptions slot="end">
+                  <IonItemOption
+                    color="danger"
+                    expandable={true}
+                    onClick={() => confirmDelete(product.id)}
+                  >
+                    <IonIcon slot="icon-only" icon={trash}></IonIcon>
+                  </IonItemOption>
+                  <IonItemOption
+                    color="warning"
+                    onClick={() => {
+                      setSelectedItemId(product.id);
+                      setShowEditAlert(true);
+                    }}
+                    // onClick={() => { setSelectedItemId(item.id); setShowEditAlert(true);
+                  >
+                    <IonIcon slot="icon-only" icon={create}></IonIcon>
+                  </IonItemOption>
+
+                  {/* Add Item to Basket */}
+                  <IonItemOption
+                    color="tertiary"
+                    onClick={() => addToBasket(product.product_price)} // Assuming product.product_price is the price of the item
+                  >
+                    <IonIcon slot="icon-only" icon={cart}></IonIcon>
+                  </IonItemOption>
+                </IonItemOptions>
+              </IonItemSliding>
+            ))}
+        </IonList>
+
+        {/* Alert to delete product */}
+        <IonAlert
+          isOpen={showDeleteAlert}
+          onDidDismiss={() => setShowDeleteAlert(false)}
+          header="Confirm Delete"
+          message="Are you sure you want to delete this item?"
+          buttons={[
+            {
+              text: "Cancel",
+              role: "cancel",
+              handler: () => {
+                setShowDeleteAlert(false); // Optionally reset itemToDelete here if not done on dismissal
+              },
+            },
+            {
+              text: "OK",
+              handler: () => {
+                if (itemToDelete !== null) {
+                  deleteItem(itemToDelete);
+                  setItemToDelete(null); // Reset the itemToDelete after deletion
+                }
+              },
+            },
+          ]}
+        />
+
+        {/* Alert to change price */}
+        <IonAlert
+          isOpen={showEditAlert}
+          onDidDismiss={() => setShowEditAlert(false)}
+          header="Update Price"
+          inputs={[
+            {
+              name: "productPrice",
+              type: "number",
+              placeholder: "Price",
+            },
+          ]}
+          buttons={[
+            {
+              text: "Cancel",
+              role: "cancel",
+              handler: () => setShowEditAlert(false),
+            },
+            {
+              text: "OK",
+              handler: (alertData) => {
+                // Assuming the new price is provided, call the update function here
+                if (selectedItemId !== null) {
+                  updateProductPrice(selectedItemId, alertData.productPrice);
+                }
+              },
+            },
+          ]}
+        />
+
+        {/* Alert to add new Item */}
+        <IonAlert
+          isOpen={showAddAlert}
+          onDidDismiss={() => setShowAddAlert(false)}
+          header="Add Item"
+          inputs={[
+            {
+              name: "productName", // Add a name property for identification
+              type: "text",
+              placeholder: "Product Name",
+            },
+            {
+              name: "productPrice",
+              type: "number",
+              placeholder: "Price",
+            },
+          ]}
+          buttons={[
+            {
+              text: "Cancel",
+              role: "cancel",
+              handler: () => {
+                console.log("Cancel clicked");
+              },
+            },
+            {
+              text: "OK",
+              handler: (alertData) => {
+                addItem(alertData.productName, alertData.productPrice);
+              },
+            },
+          ]}
+        />
+
+        <IonToast
+          isOpen={showToast}
+          onDidDismiss={() => {
+            setShowToast(false);
+            setTotalPrice(0);
+          }} // Reset total on dismiss
+          message={`Total: $${totalPrice.toFixed(2)}`}
+          duration={0} // Adjust duration as needed
+          cssClass="custom-toast" // Apply the custom CSS class
+          buttons={[
+            {
+              text: "Dismiss",
+              role: "cancel",
+              handler: () => {
+                console.log("Dismiss clicked");
+              },
+            },
+          ]}
+        />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
